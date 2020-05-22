dbf95afde2cd786fda439f0f2f921c55343e3e00
phatdroid94 <phatdroid94@gmail.com>
2020-05-22 21:18:00 +0700

    camera

--- app/src/main/java/com/example/intentimplicit30032020/MainActivity.java
+++ app/src/main/java/com/example/intentimplicit30032020/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.intentimplicit30032020;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.security.Permission;
+import java.security.Permissions;
+
+import butterknife.BindInt;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @BindView(R.id.buttonCamera)
+    Button mBtnCamera;
+    @BindView(R.id.buttonGallery)
+    Button mBtnGallery;
+    @BindView(R.id.imageview)
+    ImageView mImage;
+    Integer REQUEST_CODE_CAMERA = 123;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mBtnCamera.setOnClickListener(v -> {
+            // Tao ra request permission
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CODE_CAMERA
+            );
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent , REQUEST_CODE_CAMERA);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK && data != null){
+            Bitmap  bitmap  = (Bitmap) data.getExtras().get("data");
+            mImage.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/example/intentimplicit30032020/Sinhvien.java
@@ -0,0 +1,14 @@
+package com.example.intentimplicit30032020;
+
+public class Sinhvien {
+    String name;
+    Integer age;
+
+    public Sinhvien(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Sinhvien() {
+    }
+}
